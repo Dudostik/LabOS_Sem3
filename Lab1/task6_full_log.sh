bb6d8660045aef35f21d618bd5c3a38e7c88e192
Dudostik <57317343+Dudostik@users.noreply.github.com>
2021-04-01 07:04:42 +0300

    Update task6_full_log.sh

--- Lab1/task6_full_log.sh
+++ Lab1/task6_full_log.sh
@@ -2,5 +2,5 @@
 
 info="$(cat /var/log/anaconda/X.log | awk '$3 == "(II)"')"
 warning="$(cat /var/log/anaconda/X.log | awk '$3 == "(WW)"')"
-echo "${warning//"(WW)"/"Warning: "}" > info.log
-echo "${info//"(II)"/"Information: "}" >> info.log
+echo "${warning//"(WW)"/"Warning: "}" > full.log
+echo "${info//"(II)"/"Information: "}" >> full.log
