90c879ed6d4783dc2eeee184ae9578be65b5f9ae
Dudostik <57317343+Dudostik@users.noreply.github.com>
2021-04-02 10:53:54 +0300

    Update copy.bat

--- Lab6/Second/copy.bat
+++ Lab6/Second/copy.bat
@@ -1,1 +1,1 @@
-forfiles /P "C:\Windows" /C "cmd /c if @fsize GEQ 2097152 copy @path \\192.168.88.230\temp /Z"
+forfiles /P "C:\Windows" /C "cmd /c if @fsize GEQ 2097152 copy @path \\192.168.56.1\temp /Z"
