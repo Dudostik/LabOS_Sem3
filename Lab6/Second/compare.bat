8f08098e2bb8ba898de7805653ded2cb791d70c5
Dudostik <57317343+Dudostik@users.noreply.github.com>
2021-04-02 10:53:36 +0300

    Update compare.bat

--- Lab6/Second/compare.bat
+++ Lab6/Second/compare.bat
@@ -1,2 +1,2 @@
-cmd /c "fc C:\Windows\explorer.exe \\192.168.88.230\temp\explorer.exe > C:\Lab6\Second\log.txt"
-cmd /c "fc C:\Windows\out.mp4 \\192.168.88.230\temp\out.mp4 >> C:\Lab6\Second\log.txt"
+cmd /c "fc C:\Windows\explorer.exe \\192.168.56.1\temp\explorer.exe > C:\Lab6\Second\log.txt"
+cmd /c "fc C:\Windows\out.mp4 \\192.168.56.1\temp\out.mp4 >> C:\Lab6\Second\log.txt"
